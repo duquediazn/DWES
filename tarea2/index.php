843628c9934c3f9e805c8f2678832bf56f571afc
“nazaret” <duquediazn@gmail.om>
2024-09-25 22:00:07 +0100

    php hecho y corregido

--- tarea2/index.php
+++ tarea2/index.php
@@ -1,28 +1,57 @@
 <?php 
-    if (isset($_GET["nombre"]) && $_GET["nombre"]!="") {
-        $nombre = $_GET["nombre"];
-        $telefono = $_GET["telefono"];
-        
-        include "agenda.php";
-        
-        if($telefono!=""){
-            $agenda[$nombre]=$telefono;
-        } else {
-            $agenda[$nombre]="";
-        }
+    session_start();
 
-        foreach($agenda as $contacto=>$numero) {
-            echo $contacto.": ".$numero."<br>";
-        }
+    // Inicializar la agenda si no existe
+    if (!isset($_SESSION['agenda'])) {
+        $_SESSION['agenda']=array();
     }
 
-    /*if($GET["limpiar"]==="1") {
-        $agenda=[];
-    } else {
-        foreach($agenda as $contacto=>$numero) {
-            echo $contacto.": ".$numero."<br>";
-        }
-    }*/
+    // Procesar el formulario al enviar
+    $mensaje = "";
+    if (isset($_POST["nombre"])) {
+        $nombre = trim($_POST["nombre"]);
+        $telefono = trim($_POST["telefono"]);
+
+        //Validación datos:
+        if($_POST["nombre"]=="") {
+            $mensaje = "El nombre no puede estar vacío.";
+        } elseif(strlen($nombre) > 50) {
+            $mensaje = "El nombre no puede tener más de 30 caracteres.";
+        } elseif($telefono!="" && !preg_match('/^(6|7)[0-9]{8}$/', $telefono)) {
+            $mensaje = "El número de teléfono no es válido.";
+        } else { //Datos válidos:
+ 
+            //Se comprueba si el nombre ya existe en la agenda:
+            if(array_key_exists($nombre, $_SESSION['agenda'])) {
+                // Si el nombre ya existe
+                if ($telefono==="") {
+                    // Eliminar la entrada si no hay número
+                    unset($_SESSION['agenda'][$nombre]);
+                    $mensaje = "Contacto '$nombre' eliminado.";
+                } else {
+                    // Sustituir el número
+                    $_SESSION['agenda'][$nombre] = $telefono;
+                    $mensaje = "Número de '$nombre' actualizado.";
+                }
+            } else {
+                // Si el nombre no existe
+                $_SESSION['agenda'][$nombre] = $telefono;
+                if ($telefono!="") {
+                    $mensaje = "Contacto '$nombre' añadido.";
+                } else {
+                    $mensaje = "Contacto '$nombre' añadido sin número de teléfono.";
+                }
+                }
+            }
+    } 
+
+    //Limpiar la agenda de contactos:
+    if(isset($_GET["limpiar"]) && $_GET["limpiar"]==="1") {
+        $_SESSION['agenda'] = array();
+        $mensaje = "Todos los contactos han sido eliminados.";
+    } 
+
+    $agenda = $_SESSION['agenda'];        
 ?>
 
 <!DOCTYPE html>
@@ -35,27 +64,44 @@
 </head>
 <body>
     <h1>Agenda</h1>
+    <section class="contenedor feedback">
+    <?php
+        if($mensaje!="") {
+            echo "<p>$mensaje</p>";
+        }
+    ?>
+    </section>
     <section class="contenedor">
-        <form action="" method="">
+        <form action="" method="POST">
             <fieldset>
                 <legend>Datos Agenda</legend>
-                <?php
-                
-                ?>
+                <ul>
+                    <?php 
+                    if(!empty($agenda)) {
+                        foreach($agenda as $nombre=>$telefono) { 
+                    ?>
+                    
+                    <li> <?php echo $nombre.": ".$telefono; ?> </li>
+
+                    <?php 
+                        }
+                    }
+                    ?>
+                </ul>
             </fieldset>
             <fieldset>
                 <legend>Nuevo Contacto</legend>  
                 
                 <label for="name">Nombre: 
-                    <input type="text" name="nombre" id="name" required>
+                    <input type="text" name="nombre" id="name" maxlength="50" required>
                 </label>
                 <br>
                 <label for="telefono">Teléfono: 
-                    <input type="tel" name="telefono" id="telefono">
+                    <input type="text" name="telefono" id="telefono" pattern="^(6|7)[0-9]{8}$">
                 </label>
                 <br>
-                <button type="submit">Añadir Contacto</button>
-                <button type="reset">Limpiar Campos</button>
+                <input class="btn" type="submit" value="Añadir Contacto">
+                <input class="btn" type="reset" value="Limpiar Campos">
             </fieldset>
         </form>
         <form action="" method="GET">
@@ -63,7 +109,8 @@
                 <legend>Vaciar Agenda</legend>
 
                 <input type="hidden" name="limpiar" value="1">
-                <button type="submit">Vaciar</button>
+               
+                <input class="btn" type="submit" value="Vaciar">
             </fieldset>
         </form>
     </section>

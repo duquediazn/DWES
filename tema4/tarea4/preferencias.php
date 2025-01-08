bc8746413d1e655665ab93725e0d6a33f4e690e5
duquediazn <duquediazn@gmail.com>
2025-01-08 18:00:03 +0000

    tarea4 mond

--- tema4/tarea4/preferencias.php
+++ tema4/tarea4/preferencias.php
@@ -15,8 +15,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 }
 
 // Aquí se definen los valores predeterminados
-//$idioma = isset($_SESSION['idioma']) ? $_SESSION['idioma'] : "";
-$idioma = isset($_SESSION['idioma']) ?: "";
+$idioma = isset($_SESSION['idioma']) ? $_SESSION['idioma'] : "";
 $perfil_publico = isset($_SESSION['perfil_publico']) ? $_SESSION['perfil_publico'] : "";
 $zona_horaria = isset($_SESSION['zona_horaria']) ? $_SESSION['zona_horaria'] : "";
 ?>
